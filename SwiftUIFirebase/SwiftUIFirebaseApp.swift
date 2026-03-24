a2ac5b99c8ca479a68a52bd4bfc04c486d7fa435
rajibtalukder <rajibtalukder392@gmail.com>
2026-03-25 01:31:32 +0600

    Initial Commit

---
+++ SwiftUIFirebase/SwiftUIFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseApp.swift
+//  SwiftUIFirebase
+//
+//  Created by MacBook on 25/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
